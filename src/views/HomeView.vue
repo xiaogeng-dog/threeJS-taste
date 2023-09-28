df9c1b6913b8ff7936ff87e7fbabdb26761cc36e
gengningsheng <359315634@qq.com>
2023-09-28 16:36:37 +0800

    threejs项目渲染

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,66 @@
+<template>
+  <div></div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import gsap from 'gsap'
+const COLLISION_SCENE_URL = new URL('../assets/models/scene_collision.glb', import.meta.url).href
+const STATIC_SCENE_URL = new URL('../assets/models/scene_desk_obj.glb', import.meta.url).href
+
+// 初始化场景
+const scene = new THREE.Scene()
+// 初始化相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(-3.23, 2.98, 4.06)
+// camera.aspect = window.innerWidth / window.innerHeight
+// 更新投影矩阵
+camera.updateProjectionMatrix()
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer({
+  // 设置抗锯齿
+  antialias: true
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+// 初始化控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+// 初始化loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('./draco/')
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+// 加载模型
+gltfLoader.load('./model/scene.glb', (gltf) => {
+  scene.add(gltf.scene)
+})
+
+// 添加平行光
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 50, 0)
+scene.add(light)
+
+function render() {
+  requestAnimationFrame(render)
+  renderer.render(scene, camera)
+  controls.update()
+}
+render()
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="less" scoped>
+canvas {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import AutoImport from 'unplugin-auto-import/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      // 可以自定义文件生成的位置，默认是根目录下，使用ts的建议放src目录下
+      dts: true,
+      imports: ['vue', 'vue-router'],
+      eslintrc: {
+        enabled: false // 1、改为true用于生成eslint配置。2、生成后改回false，避免重复生成消耗
+      }
+    })
   ],
   resolve: {
     alias: {
